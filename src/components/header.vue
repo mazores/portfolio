d569a09f5d33293ae4cf0662e54c2f048f4656b0
Mikka A <mikka@Mikka.local>
2020-04-06 03:22:46 -0400

    Tablet and Desktop Nav

--- src/components/header.vue
+++ src/components/header.vue
@@ -18,7 +18,7 @@
       </div>
     </div>
 
-    <nav class="scrolling-nav"
+    <nav class="scrolling-nav-mobile"
       data-aos="fade-down"
       data-aos-anchor="#trigger-nav"
       data-aos-anchor-placement="center-center">
@@ -60,7 +60,7 @@
       </ul>
     </nav>
 
-    <!-- <nav class="scrolling-nav"
+    <nav class="scrolling-nav"
       data-aos="fade-down"
       data-aos-anchor="#trigger-nav"
       data-aos-anchor-placement="center-center">
@@ -75,7 +75,7 @@
         <li><a href="#" v-scroll-to="'#testimonials'">What People Say</a></li>
         <li><a href="#" v-scroll-to="'#contact'">Contact</a></li>
       </ul>
-    </nav> -->
+    </nav>
   </div>
 </template>
 
@@ -113,10 +113,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-
 .tablet { display: none; }
-
 nav {
   display: flex;
   flex-direction: row;
@@ -258,7 +255,7 @@ nav {
   right: 0vw!important;
 }
 
-.scrolling-nav {
+.scrolling-nav-mobile {
   z-index: 500;
   position: fixed;
   top: 0;
@@ -275,115 +272,179 @@ nav {
   }
 }
 
-@media screen and (min-width: 768px) {
-.mobile { display: none; }
-.tablet { display: block; }
+@media screen and (min-width: 375px) {
+  .mobile-open-container {
+    .mobile-open {
+      ul {
+        padding-top: 150px;
+      }
+    }
+  }  
 }
 
-// nav {
-//   display: flex;
-//   flex-direction: row;
-//   align-items: center;
-//   justify-content: space-between;
-//   padding: 50px 50px;
-//   margin-bottom: 20px;
+// TABLET
+@media screen and (min-width: 768px) {
+  .mobile, .mobile-open-container { display: none; }
 
-//   .logo {
-//     img {
-//       &:hover {
-//         -webkit-transition: all 200ms ease 0s;
-//         -webkit-transform: scale(1.1); 
-//         -ms-transition: all 200ms ease 0s;
-//         -ms-transform: scale(1.1); 
-//         -moz-transition: all 200ms ease 0s;
-//         -moz-transformz : scale(1.1);
-//         transition: all 200ms ease 0s;
-//         transform: scale(1.1);   
-//         cursor: pointer;
-//       }
-//     }
-//   }
+  .tablet {
+    display: block; 
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 50px 50px;
+    margin-bottom: 20px;
 
-//   ul {
-//     text-decoration: none;
-//     list-style-type: none;
+    .logo {
+      img {
+        width: 100%;
 
-//     li {
-//       display: inline-block;
-//       margin-left: 2.79vw;
+        &:hover {
+          -webkit-transition: all 200ms ease 0s;
+          -webkit-transform: scale(1.1); 
+          -ms-transition: all 200ms ease 0s;
+          -ms-transform: scale(1.1); 
+          -moz-transition: all 200ms ease 0s;
+          -moz-transformz : scale(1.1);
+          transition: all 200ms ease 0s;
+          transform: scale(1.1);   
+          cursor: pointer;
+        }
+      }
+    }
 
-//       a {
-//         color: $blue;
-//         font-family: $Poppins;
-//         font-size: 28px;
-//         // font-size: 1.56vw;
-//         font-weight: 700;
-//         text-decoration: none;
-//         position: relative;
-//         text-transform: lowercase;
-//         transition: color 0.5s ease 0s;
+    ul {
+      text-decoration: none;
+      list-style-type: none;
 
-//         &:hover {
-//           color: $red;
-//         }
+      li {
+        display: inline-block;
+        margin-left: 2.79vw;
 
-//         &:after {
-//           background: none repeat scroll 0 0 transparent;
-//           bottom: 5px;
-//           content: "";
-//           display: block;
-//           height: 10px;
-//           left: 0;
-//           position: absolute;
-//           background: $yellow;
-//           transition: width 0.3s ease 0s, left 0.3s ease 0s;
-//           width: 0;
-//           z-index: -3;
-//         }
+        a {
+          color: $blue;
+          font-family: $Poppins;
+          font-size: 18px;
+          // font-size: 1.56vw;
+          font-weight: 700;
+          text-decoration: none;
+          position: relative;
+          text-transform: lowercase;
+          transition: color 0.5s ease 0s;
 
-//         &:hover:after {
-//           width: 100%; 
-//           left: 0; 
-//         }
-//       }
-//     }
-//   }
-// }
+          &:hover {
+            color: $red;
+          }
 
-// .scrolling-nav {
-//   z-index: 500;
-//   position: fixed;
-//   top: 0;
-//   width: 100vw;
-//   padding: 20px 50px;
-//   background-color: $navy;
-//   opacity: 0.8;
+          &:after {
+            background: none repeat scroll 0 0 transparent;
+            bottom: 5px;
+            content: "";
+            display: block;
+            height: 10px;
+            left: 0;
+            position: absolute;
+            background: $yellow;
+            transition: width 0.3s ease 0s, left 0.3s ease 0s;
+            width: 0;
+            z-index: -3;
+          }
 
-//   .logo {
-//     img {
-//       width: 70%;
-//     }
-//   }
+          &:hover:after {
+            width: 100%; 
+            left: 0; 
+          }
+        }
+      }
+    }
+  }
 
-//   ul {
-//     li {
-//       a {
-//         font-size: 14px;
+  .scrolling-nav {
+    z-index: 500;
+    position: fixed;
+    top: 0;
+    width: 100vw;
+    padding: 20px 50px;
+    background-color: $navy;
+    opacity: 0.8;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 
-//         &:hover {
-//           color: $yellow;
-//         }
+    .logo {
+      img {
+        width: 70%;
+      }
+    }
 
-//         &:after {
-//           height: 5px;
-//           bottom: 0;
-//           background: $red;
-//         }
-//       }
-//     }
-//   }
-// }
+    ul {
+      text-decoration: none;
+      list-style-type: none;
 
+      li {
+        display: inline-block;
+        margin-left: 2.79vw;
+
+        a {
+          color: $blue;
+          font-family: $Poppins;
+          font-size: 18px;
+          // font-size: 1.56vw;
+          font-weight: 700;
+          text-decoration: none;
+          position: relative;
+          text-transform: lowercase;
+          transition: color 0.5s ease 0s;
+
+          &:hover {
+            color: $yellow;
+          }
+
+          &:after {
+            background: none repeat scroll 0 0 transparent;
+            bottom: 5px;
+            content: "";
+            display: block;
+            height: 5px;
+            left: 0;
+            position: absolute;
+            background: $red;
+            transition: width 0.3s ease 0s, left 0.3s ease 0s;
+            width: 0;
+            z-index: -3;
+          }
+
+          &:hover:after {
+            width: 100%; 
+            left: 0; 
+          }
+        }
+      }
+    }
+  }
+}
+
+// DESKTOP
+@media screen and (min-width: 1024px) {
+  .tablet {
+    ul {
+      li {
+        a {
+          font-size: 28px;
+        }
+      }
+    }
+  }
+  .scrolling-nav {
+    ul {
+      li {
+        a {
+          font-size: 18px;
+        }
+      }
+    }
+  }
+}
 // @media screen and (max-width:375px) {
 //   nav {
 //     padding: 10px 15px;
